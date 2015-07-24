959c838874422b0df76457e84174c6bda8059304
Winston Wang <winstonwang12@gmail.com>
2015-07-24 00:04:07 -0700

    auto layout doneish and conditional segue done
    this is the real sweggy

--- Onaji/LoginScreenViewController.swift
+++ Onaji/LoginScreenViewController.swift
@@ -14,19 +14,37 @@ class LoginScreenViewController: UIViewController
     var userinfo = UserInfo()
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var username: UITextField!
-    
     @IBOutlet weak var loginState: UILabel!
+    
+    private var loginSuccess: Bool = false
+    
+    private enum Constants
+    {
+        static let yesMessage = "ya"
+        static let noMessage = "no"
+    }
     
     @IBAction func userLogin()
     {
-        let loginSuccess = userinfo.testLogin(username.text,password:password.text)
-        if(loginSuccess) {
-            loginState.text = "ya"
-            performSegueWithIdentifier("loginSuccess", sender: self)
+        loginSuccess = userinfo.testLogin(username.text,password:password.text)
+        
+        if(loginSuccess)
+        {
+            loginState.text = Constants.yesMessage
+            loadDestinationVC()
         }
         else
         {
-            loginState.text = "no"
+            loginState.text = Constants.noMessage
+        }
+    }
+    
+    //This is for the segue named "loginCorrect"
+    func loadDestinationVC()
+    {
+        if loginSuccess
+        {
+            self.performSegueWithIdentifier("loginCorrect", sender:nil)
         }
     }
 }
