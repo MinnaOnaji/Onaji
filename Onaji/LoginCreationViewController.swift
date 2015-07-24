2eea1ad2fbf65d483dab7794eb3a6ff7f974ace9
matthewli1997 <matthewli1997@gmail.com>
2015-07-23 19:39:45 -0700

    added more storyboard, new view controller

---
+++ Onaji/LoginCreationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginCreationViewController.swift
+//  Onaji
+//
+//  Created by Matthew Li on 7/23/15.
+//  Copyright (c) 2015 Same. All rights reserved.
+//
+
+import UIKit
+
+class LoginCreationViewController: UIViewController {
+
+
+
+}
