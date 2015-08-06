8c4beb6d4b56a79e36794dde501b9e9f948ec526
matthewli1997 <matthewli1997@gmail.com>
2015-08-05 21:21:40 -0700

    added more parameters to users, set up an outline for friend view controller

--- FriendViewController.swift
+++ FriendViewController.swift
@@ -10,5 +10,22 @@ import UIKit
 
 class FriendViewController: UIViewController
 {
-	
+    //retrieve friends from database
+    
+	//get list of friends
+    func createFriendCells(users_friends: [User])
+    {
+        for friend in users_friends
+        {
+            //let profile_picture = friend.picture
+            let username = friend.username
+            let name = friend.firstName + " " + friend.lastName
+            let friendBio = friend.bio
+            
+            //implementation to create unique cell
+            //add to table
+        }
+    }
+    
+    //display cells
 }
--- Onaji/User.swift
+++ Onaji/User.swift
@@ -6,13 +6,26 @@
 //  Copyright (c) 2015 Same. All rights reserved.
 //
 
+//add more parameters to user, implement parameters in profile page
+//save instances of user into server, core data
+//prepare for segue
+//retrieve instances, display
+
 import Foundation
+import UIKit
 //import Keychain Matthew Li how does this work?
 
 class User
 {
     var username: String
     var password: String
+    
+    var friends: [User] = []
+    //var picture: UIImage = nil
+    
+    var firstName: String = ""
+    var lastName: String = ""
+    var bio: String = ""
     
     //Implement other settings here
     
