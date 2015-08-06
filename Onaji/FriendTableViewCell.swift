3c9fc8ba1c63c4bac580fc2c459703ae41704a6f
matthewli1997 <matthewli1997@gmail.com>
2015-08-05 21:46:07 -0700

    linked some outlets to table view storyboard, added table view cell class

---
+++ Onaji/FriendTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  FriendTableViewCell.swift
+//  Onaji
+//
+//  Created by Matthew Li on 8/5/15.
+//  Copyright (c) 2015 Same. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FriendTableViewCell : UITableViewCell
+{
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var friendFullName: UILabel!
+    
+    @IBOutlet weak var friendBio: UILabel!
+}
