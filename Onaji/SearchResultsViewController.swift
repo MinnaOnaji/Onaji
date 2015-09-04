85df88649561b9f434bec227cc63611c523e57b1
winst13 <winstonwang12@gmail.com>
2015-09-04 01:13:05 -0700

    search view controllers work and segue to each other

--- Onaji/SearchResultsViewController.swift
+++ Onaji/SearchResultsViewController.swift
@@ -10,5 +10,64 @@ import UIKit
 
 class SearchResultsViewController: UIViewController
 {
-   
+	var filteredTutors = [UserInformation]()
+	
+	@IBOutlet weak var searchedStringLabel: UILabel!
+	
+	@IBOutlet weak var searchResultsTable: UITableView!
+	
+	var searchStringDelegate: sendSearchStringDelegate?
+	
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		
+		if let searchedString = searchStringDelegate?.getString()!
+		{
+			searchedStringLabel.text = searchedString
+			filterContentForSearchText(searchedString)
+		}
+		else
+		{
+			println("no search text")
+		}
+		
+		println("filtered tutors: \(filteredTutors)")
+	}
+	
+	// prints out all UserInformations that has subject as one of their subjects
+	func findTutorWithSubject(subject: String) -> [UserInformation]
+	{
+		let query = UserInformation.query()!
+		query.whereKey("subjects", equalTo: subject)
+		var result = [UserInformation]()
+		
+		query.findObjectsInBackgroundWithBlock { (objects: [AnyObject]?, error: NSError?) -> Void in
+			if error == nil
+			{
+				if let objects = objects as? [UserInformation]
+				{
+					// objects is [UserInformation] that contains corresponding subject
+					for user in objects {
+						println(user.username!)
+						println(user.subjects)
+						result.append(user)
+					}
+				}
+			}
+		}
+		
+		println(result)
+		return result
+	}
+	
+	func filterContentForSearchText(searchText: String)
+	{
+		filteredTutors = findTutorWithSubject(searchText)
+	}
+}
+
+protocol sendSearchStringDelegate
+{
+	func getString() -> String?
 }
